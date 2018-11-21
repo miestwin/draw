4da47e3a745b995be36701caf22ca68404651ba0
Radosław Chełstowski <chelstowski.radoslaw@gmail.com>
2018-11-21 23:09:04 +0100

    Change script

--- public/js/script.js
+++ public/js/script.js
@@ -1,26 +1,40 @@
-var penColorDefault = '#ffffff';
-var penColor = penColorDefault;
-var MIN_PEN_WIDTH = 2;
-var MAX_PEN_WIDTH = 14;
-var penWidthDefault = 2;
-var penWidth = penWidthDefault;
-
 document.addEventListener('contextmenu', event => event.preventDefault());
 
-function increasePen() {
-    if (penWidth < MAX_PEN_WIDTH) {
-        penWidth += 1;
-        updateIndicator();
-    }
-}
+const Draw = (function(window, document, Hammer, paper) {
 
-function decreasePen() {
-    if (penWidth > MIN_PEN_WIDTH) {
-        penWidth -= 1;
-        updateIndicator();
-    }
-}
+    const penColorDefault = '#ffffff';
+    let penColor = penColorDefault;
 
-function updateIndicator() {
-    document.querySelector('.c-menu__item--indicator').textContent = penWidth;
-}
+    const MIN_PEN_WIDTH = 2;
+    const MAX_PEN_WIDTH = 14;
+    const penWidthDefault = 2;
+    let penWidth = penWidthDefault;
+
+    const canvas = document.getElementById('draw');
+
+    function increasePen() {
+        if (penWidth < MAX_PEN_WIDTH) {
+            penWidth += 1;
+            updateIndicator();
+        }
+    }
+
+    function decreasePen() {
+        if (penWidth > MIN_PEN_WIDTH) {
+            penWidth -= 1;
+            updateIndicator();
+        }
+    }
+
+    function updateIndicator() {
+        document.querySelector('.c-menu__item--indicator').textContent = penWidth;
+    }
+
+    const mc = new Hammer.Manager(canvas);
+
+
+    return {
+        increasePen,
+        decreasePen,
+    }
+})(window, document, Hammer, paper);
