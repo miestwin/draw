b516b64d64b541b860e0d2a11634432d84704a31
Radosław Chełstowski <chelstowski.radoslaw@gmail.com>
2018-11-25 14:27:03 +0100

    Add drawing feature

--- README.md
+++ README.md
@@ -5,5 +5,13 @@
 - download image
 - menu with colors, trash, clear
 - save in mysql
+- socket
 - unique url
 - docker
+- responsive
+- canvas resize
+
+- 1 finger draw (mouse)
+- 2 fingers erase (mouse + shift)
+- 3 finger drag (mouse + ctrl)
+- 2 fingers zoom (scroll)
--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,36 @@
-document.addEventListener('contextmenu', event => event.preventDefault());
 
 const Draw = (function(window, document, Hammer, paper) {
+    /**
+     * Prevent showing context menu (right mouse click)
+     */
+    document.addEventListener('contextmenu', event => event.preventDefault());
 
+    let path;
+    let lastActionName;
+
+    /**
+     * Pen color
+     */
     const penColorDefault = '#ffffff';
     let penColor = penColorDefault;
 
+    /**
+     * Pen width
+     */
     const MIN_PEN_WIDTH = 2;
     const MAX_PEN_WIDTH = 14;
     const penWidthDefault = 2;
     let penWidth = penWidthDefault;
 
+    /**
+     * Setup paper
+     */
     const canvas = document.getElementById('draw');
+    paper.setup(canvas);
 
+    /**
+     * Increase pen width
+     */
     function increasePen() {
         if (penWidth < MAX_PEN_WIDTH) {
             penWidth += 1;
@@ -19,6 +38,9 @@ const Draw = (function(window, document, Hammer, paper) {
         }
     }
 
+    /**
+     * Decrease pen width
+     */
     function decreasePen() {
         if (penWidth > MIN_PEN_WIDTH) {
             penWidth -= 1;
@@ -26,12 +48,106 @@ const Draw = (function(window, document, Hammer, paper) {
         }
     }
 
+    /**
+     * Update text with pen width
+     */
     function updateIndicator() {
         document.querySelector('.c-menu__item--indicator').textContent = penWidth;
     }
 
+    /**
+     * Start drawing on canvas
+     * @param {*} event 
+     */
+    function startDraw(event) {
+        lastActionName = 'draw';
+        path = new paper.Path({
+            segments: [{ x: event.center.x, y: event.center.y }],
+            strokeColor: penColor,
+            strokeWidth: penWidth,
+            strokeCap: 'round',
+            fullySelected: false
+        });
+    }
+
+    /**
+     * Draw on canvas
+     * @param {*} event 
+     */
+    function draw(event) {
+        path.add({ x: event.center.x, y: event.center.y });
+    }
+
+    /**
+     * Stop drawing on canvas
+     * @param {*} event 
+     */
+    function endDraw(event) {
+        path.simplify(8);
+    }
+
     const mc = new Hammer.Manager(canvas);
 
+    const pan = new Hammer.Pan({ pointers: 3 });
+    mc.add(pan);
+
+    mc.on('hammer.input', function (event) {
+        if (event.srcEvent.ctrlKey) {
+            event.maxPointers = 3;
+        } else if (event.srcEvent.shiftKey) {
+            event.maxPointers = 2;
+        }
+
+        if (event.isFirst) {
+            handleFirstEvent(event);
+        } else if (event.isFinal) {
+            handleFinalEvent(event);
+        } else {
+            handleEvent(event);
+        }
+    });
+
+    function handleFirstEvent(event) {
+        switch (event.maxPointers) {
+            case 1:
+                startDraw(event);
+                break;
+            case 2:
+                // start erase
+                break;
+            case 3:
+                // start drag
+                break;
+        }
+    }
+
+    function handleEvent(event) {
+        switch (lastActionName) {
+            case 'draw':
+                draw(event);
+                break;
+            case 'erase':
+                // handle erase
+                break;
+            case 'drag':
+                // handle drag
+                break;
+        }
+    }
+
+    function handleFinalEvent(event) {
+        switch (lastActionName) {
+            case 'draw':
+                endDraw(event);
+                break;
+            case 'erase':
+                // handle erase end
+                break;
+            case 'drag':
+                // handle drag end
+                break;
+        }
+    }
 
     return {
         increasePen,
